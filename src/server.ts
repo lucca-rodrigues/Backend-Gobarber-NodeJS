639eb5232bcc4c1eec97d0ad74064b09eed1e77b
Lucca Rodrigues <lucasrod2008rigues@gmail.com>
2020-04-21 12:28:43 -0300

    Configurações do servidor

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import routes from './Routes';
+
+const app = express();
+
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log('✅ Server started on port 3333');
+});
